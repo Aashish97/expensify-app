48d95be7055f6c053cee7ea14d692269c911fa9b
aashish <aashish@ubuntu>
2020-04-06 12:02:29 +0545

    Foundamentals of react router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css/normalize.css"
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>Boiler plate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => <div>Dashboard Page</div>;
+
+const AddExpensePage = () => <div>Add Expense Page</div>;
+
+const EditExpensePage = () => <div>Edit Expense Page</div>;
+
+const HelpExpensePage = () => <div>Help Expense Page</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpExpensePage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
