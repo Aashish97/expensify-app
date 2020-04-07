4d94f3644316f15286dd0978a8d74d7dbfb91bef
aashish <aashish@ubuntu>
2020-04-07 21:05:26 +0545

    Added create expense form component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
-const AddExpensePage = () => <div>Add Expense Page</div>;
+const AddExpensePage = () => (
+  <div>
+  <h3>Add Expense</h3>
+    <ExpenseForm />
+  </div>
+);
 
 export default AddExpensePage;
