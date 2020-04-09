39213da08f0c7165c3f36fc9b9fc38be2712e7d9
aashish <aashish@ubuntu>
2020-04-09 08:11:35 +0545

    Minor fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,11 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => (
-    <div>
+    <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </div>
+    </header>
 );
 
 export default Header
