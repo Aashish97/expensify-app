f2291dd670f185c9505f602ba53a79312e02b032
aashish <aashish@ubuntu>
2020-04-11 19:49:45 +0545

    Added the track of the user

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
 import { startSetExpenses } from "./actions/expenses";
+import { login, logout } from "./actions/auth";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
@@ -29,13 +30,15 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
-      if(history.location.pathname === "/"){
-        history.push("/dashboard")
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push("/");
   }
