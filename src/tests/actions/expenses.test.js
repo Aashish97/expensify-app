2ee910ed493981c7785c1dc28931ee53b977e33c
aashish <aashish@ubuntu>
2020-04-11 15:15:01 +0545

    Added test-cases for editing the expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -8,6 +8,7 @@ import {
   setExpenses,
   startSetExpenses,
   startRemoveExpense,
+  startEditExpense,
 } from "./../../actions/expenses";
 import expenses from "./../fixtures/expenses";
 import database from "./../../firebase/firebase";
@@ -61,6 +62,29 @@ test("Should setup edit expense action object", () => {
       note: "New note added",
     },
   });
+});
+
+test("should edit expense from firebase", (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = {
+    amount: 12345,
+  };
+  store
+    .dispatch(startEditExpense(id, updates))
+    .then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: "EDIT_EXPENSE",
+        id,
+        updates,
+      });
+      return database.ref(`expenses/${id}`).once("value");
+    })
+    .then((snapshot) => {
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
+    });
 });
 
 test("Should setup add expense action object with provided values", () => {
